46748c49d5c1a54aae8da282e1e93efbe9a00df3
pubkey <daniel_van_meyer@hotmail.de>
2020-03-06 23:56:10 +0100

    ADD updated minimal bdd

--- javascript/src/bdd/bdd.generated.ts
+++ javascript/src/bdd/bdd.generated.ts
@@ -25,7 +25,7 @@ import {
     doesMatchNow
 } from '../states/state-resolver';
 
-export const minimalBddString = '11a2b0c3d)e*f/g.h-i+j4k(lbh,mjh,nch,ojk,pgh,qkj,rjo-snj-tcj-upj-vej-wdj-xpq-yki-zkj-{aw5|ou5}mk5~ru5ms5¡jt5¢ju5£jw5¤ox5¥zy5¦jk5§ju3¨jw3©¤}3ªj~3«j£3¬¥j3­zj3®l1¯b¡1°§ª1±¨«1²k©1³k¦1´­¬1µ²¢4¶­k4·jk4¸´k4¹{|0º¯®0»ek0¼±°0½³µ0¾h¶0¿i¸0À¡0Áfj0Âcj0Ã£¢0Ävz0Åkz0Æ¹»6ÇºÂ6È¼»6É½¾6Ê¿·6ËÀÂ6ÌÁj6ÍÃÄ6ÎzÅ6ÏÈÆ(Ðwk7ÑÏÉ7ÒkÊ7ÓkÐ8ÔÒÑ8ÕÌÇ8ÖÌË8×ÎÍ8Økw8ÙÔÓ.Ú×Ø.ÛÙÕ2ÜÚÖ2ÛÜ/';
+export const minimalBddString = '11a2b0c3d)e*f/g.h-i+j4k(lkj3mjk3nij3ojm1pbj1qkj1rkm1skl1tjn1uja(vok(wuj/xvj/yej/zkj/{ey-|jw-}dj-~jx-gj-¡hj-¢cj-£ks-¤kz-¥kj-¦hr-§gk-¨kt-©{h7ª|q7«jp7¬jq7­}k7®¡¦7¯§7°~£7±¤j7²¤i7³¤¥7´¤¨7µ«h,¶¬°,·¯®,¸¡,¹¢¡,ºª­5»«¢5¼µ¹5½¶·5¾j¸5¿³´5Àº©6Á»c6Â¼j6Ã½³6Ä¾k6Å²k6Æ¿±6ÇÅ²*ÈÆ³*ÉÀÃ0ÊÁÂ0ËeÄ0Ìfj0ÍÇÈ0ÎÉ­.ÏÍk.ÐÎÊ2ÑÏÌ2ÒÐË4ÓÑk4ÓÒ8';
 export const stateResolvers: ResolverFunctions<StateResolveFunctionInput<any>> = {
     0: isInsert,
     1: isUpdate,
