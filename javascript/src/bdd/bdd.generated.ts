7dfd38b2ef3ba75a8b7b7f0c3b69936bd0965d69
pubkey <daniel_van_meyer@hotmail.de>
2020-03-17 01:12:33 +0100

    0.2.1

--- javascript/src/bdd/bdd.generated.ts
+++ javascript/src/bdd/bdd.generated.ts
@@ -25,7 +25,7 @@ import {
     doesMatchNow
 } from '../states/state-resolver';
 
-export const minimalBddString = '11a+b0c/d3e.f2g*h-i)j(k4lfk-mgk-ndk-ojk-pek-qhk-rik-sbd1tsh,udh,vnq,wpq,xst0yfk0zgj0{lk0|mo0}ck0~du0dk0¡nv0¢ak0£hk0¤rw0¥x6¦yz6§{|6¨~6©k£6ªkj6«¦z4¬«¤5­¦¤5®¥¡5¯jo5°§¤5±¨¡5²©j5³ªw5´¯o*µj´/¶¬°/·µj3¸j¢3¹¶­3º³²3»·}2¼¸}2½¹±2¾º®2¿»¼7À½¾7Árj7Â¿À8ÃjÁ8ÂÃ.';
+export const minimalBddString = '11a+b0c/d3e.f2g*h-i)j(k4lbh,meh,ndh,ofg6pkh6qkj6rnk6sok-tdk-uqk-vjk-wmk-xnk-yik-zoy5{qw5|sy5}dt5~jv5uw5¡rx5¢pj5£kj5¤oz1¥yz1¦b}1§q{1¨j{1©w{1ªl¡1«j¢1¬j£1­¤¥3®§©3¯jk3°¨¬3±~v*²j±/³­|/´®/µ²c2¶ac2·²k2¸¯k2¹³}2º«¦2»´¡2¼°ª2½µj.¾¶j.¿¹y.Àºj.Á½¾7Â·¸7Ã¿À7Ä»¼7ÅÁÃ8ÆÂÄ8ÅÆ0';
 export const stateResolvers: ResolverFunctions<StateResolveFunctionInput<any>> = {
     0: isInsert,
     1: isUpdate,
