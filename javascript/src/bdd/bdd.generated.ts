15061fe98be8e45b9253fc7c0b09c6568dcb9e1d
pubkey <daniel_van_meyer@hotmail.de>
2020-04-03 10:44:07 +0200

    ADD new optimized bdd

--- javascript/src/bdd/bdd.generated.ts
+++ javascript/src/bdd/bdd.generated.ts
@@ -3,10 +3,11 @@ import {
     minimalStringToSimpleBdd,
     resolveWithSimpleBdd
 } from 'binary-decision-diagram';
+
 import type { StateResolveFunctionInput } from '../types';
 import { stateResolveFunctionByIndex } from '../states';
 
-export const minimalBddString = '11a+b0c/d3e.f2g*h-i)j(k4lkh6mdk6nhk6ofg6pkj6qfg4rkj4smd5tlj5udk5vmk5wnh5xkj5ynk5zoi5{pe5|ok5}pk5~jh5jk5¡r{7¢ja7£|t7¤rj7¥r}7¦}x7§j~7¨j7©jk7ªx7«kj7¬jk-­j¢-®z£-¯e«-°¡¥-±{¦-²zk-³du-´{k-µsv-¶j©-·h«-¸§¨-¹~ª-ºj-»~k-¼wy-½i«-¾z|-¿{}-À~-Áik-Â½®1Ãb³1Ä¤°1Å¯±1Æbµ1Çj¸1È·¹1Éh¼1Ê¬c2Ë¬k2Ìjc2ÍqÃ2ÎÄÆ2Ïºc2Ð²³2Ñ´µ2Òjk2ÓÇÉ2Ôºk2Õ»¼2ÖÎÓ,×ÅÈ,ØÑÕ,Ù¿À,ÚÌ­3ÛÍÂ3ÜÒ¶3ÝÖ×3ÞÚÊ/ßÜË/à½Á/áÚÏ/âÛÐ/ãÜÔ/äÝØ/åÁà)æjá)ç¾â)èjã)éÙä)êæÞ*ëèß*ìêë0íçé0îêç8ïjå8ðìí8ñîð+ñï.';
+export const minimalBddString = '11a+b0c/d3e.f2g*h-i)j(k4lfk/mjk/nkg4omj4pkj4qlf3rng3soj3tpj3ujk3vkj3wqr6xkh6ykt6zku6{dk6|hk6}fg6~kj6{d5¡wk5¢xj5£js5¤yk5¥zv5¦dk5§{k5¨|h5©jt5ªtk5«ju5¬uv5­|k5®}i5¯~e5°}k5±~k5²jh5³jk5´bd1µj¯1¶b1·°¡1¸j¢1¹b¦1ºj£1»±¤1¼j¥1½b§1¾j²1¿h¨1Àj©1Á³ª1Â³«1Ãj¬1Äh­1Å°·)Æjº)Ç±»)ÈjÀ)É³Á)Êsa7Ësu7Ìtu7Íd´7ÎÆa7ÏÅ¸7Ð¦¹7Ñ¯µ7Ò¶7ÓÆÂ7ÔÇ¼7Õ§½7Ö²¾7×¨¿7ØÈÂ7ÙÉÃ7Ú­Ä7Ûkj7ÜÊË0Ý®Ñ0ÞÍÒ0ßÎÓ0àÏÔ0áck0âÐÕ0ãÜÌ,äÝÖ,åák,æÞ×,çßØ,èàÙ,éâÚ,êjã-ëjç-ìäè-íæé-îiÛ-ïëì8ðåí8ñjî8òêå2óïð2ôòj.õóñ.õô*';
 
 export const simpleBdd: SimpleBdd = minimalStringToSimpleBdd(minimalBddString);
 export const resolveInput = (input: StateResolveFunctionInput<any>) => resolveWithSimpleBdd(
