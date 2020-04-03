9dce450253d338678657b1167a2b19197c39a973
pubkey <daniel_van_meyer@hotmail.de>
2020-04-03 18:41:47 +0200

    ADD Todo

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# TODO
+
+
+
+## Add a state function 'wasSelectorFieldChanged'
+That returns true if a field was changed the is used in the selector.
+This helps to optimized when `skip` is used an a hidden document is updated.
--- javascript/src/truth-table-generator/runner.node.ts
+++ javascript/src/truth-table-generator/runner.node.ts
@@ -212,7 +212,6 @@ async function run() {
                 const perfMeasurement = await measurePerformanceOfStateFunctions(10000);
                 console.log('state function performance:');
                 console.dir(perfMeasurement);
-    
 
                 await optimizeBruteForce({
                     truthTable,
